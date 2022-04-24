0e21b0224078a03fbf93c7dab78144d8d27062e6
Carlos Diaz <carlos.diaz@outlook.fr>
2022-04-24 23:59:59 +0200

    new changes

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=hypnos;charset=utf8mb4;port=3306', 'root', 'palante');
+    $pdo = new PDO('mysql:host=localhost;dbname=hypnos;charset=utf8mb4;port=3306', 'root', '');
 } catch (PDOException $e) {
     echo 'La connexion à la base de données a échoué';
     $e->getMessage();
