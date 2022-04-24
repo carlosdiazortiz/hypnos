6e4a8515204fcb8ac5323281be4ca95c1d8dce56
Carlos Diaz <carlos.diaz@outlook.fr>
2022-04-24 14:22:24 +0200

    more and more database connection changes

--- database_connection.php
+++ database_connection.php
@@ -1,5 +1,7 @@
 <?php
-
-    $pdo = new PDO('postgresql://db:AVNS_uOuk3Fz2sFf00J_@app-378459c1-480f-4286-b61f-dcf36c55500d-do-user-11421380-0.b.db.ondigitalocean.com:25060/db?sslmode=require',
-        'db',
-        'AVNS_uOuk3Fz2sFf00J_');
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=hypnos;charset=utf8mb4;port=3306', 'admin', 'hippocampe');
+} catch (PDOException $e) {
+    echo 'La connexion à la base de données a échoué';
+    $e->getMessage();
+}
