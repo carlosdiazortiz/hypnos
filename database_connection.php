1920a694dc2739fe915d5e325dbb264d4ec3799c
Carlos Diaz <carlos.diaz@outlook.fr>
2022-04-24 14:14:37 +0200

    more changes to database connection

--- database_connection.php
+++ database_connection.php
@@ -1,9 +1,5 @@
 <?php
-try {
+
     $pdo = new PDO('postgresql://db:AVNS_uOuk3Fz2sFf00J_@app-378459c1-480f-4286-b61f-dcf36c55500d-do-user-11421380-0.b.db.ondigitalocean.com:25060/db?sslmode=require',
         'db',
         'AVNS_uOuk3Fz2sFf00J_');
-} catch (PDOException $e) {
-    echo 'La connexion à la base de données a échoué';
-    $e->getMessage();
-}
