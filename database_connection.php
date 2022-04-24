04b65eb04c3e1ff8b6edcd74ae01ee7a0681e3e2
Carlos Diaz <carlos.diaz@outlook.fr>
2022-04-24 13:56:39 +0200

    update connection parameters

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,11 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=hypnos;charset=utf8mb4;port=3306', 'admin', 'hippocampe');
+    $pdo = new PDO('mysql:host=app-378459c1-480f-4286-b61f-dcf36c55500d-do-user-11421380-0.b.db.ondigitalocean.com;
+    dbname=db;
+    charset=utf8mb4;
+    port=25060',
+        'db',
+        'AVNS_uOuk3Fz2sFf00J_');
 } catch (PDOException $e) {
     echo 'La connexion à la base de données a échoué';
     $e->getMessage();
